2b75d66f47dd690797bc7969b99d7d4e8f659005
thebestdev <thebestdeveloper@outlook.fr>
2024-05-23 21:40:08 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 // YouTube Awesome V1.0
 
+console.log(" __   __  _______  __   __  _______  __   __  _______  _______    _______  _     _  _______  _______  _______  __   __  _______ ")
+console.log("|  | |  ||       ||  | |  ||       ||  | |  ||  _    ||       |  |   _   || | _ | ||       ||       ||       ||  |_|  ||       |")
+console.log("|  |_|  ||   _   ||  | |  ||_     _||  | |  || |_|   ||    ___|  |  |_|  || || || ||    ___||  _____||   _   ||       ||    ___|"
+console.log("|       ||  | |  ||  |_|  |  |   |  |  |_|  ||       ||   |___   |       ||       ||   |___ | |_____ |  | |  ||       ||   |___ ")
+console.log("|_     _||  |_|  ||       |  |   |  |       ||  _   | |    ___|  |       ||       ||    ___||_____  ||  |_|  ||       ||    ___|")
+console.log("  |   |  |       ||       |  |   |  |       || |_|   ||   |___   |   _   ||   _   ||   |___  _____| ||       || ||_|| ||   |___ ")
+console.log("  |___|  |_______||_______|  |___|  |_______||_______||_______|  |__| |__||__| |__||_______||_______||_______||_|   |_||_______|")
+console.log("Loading please...")
+
 document.dispatchEvent(new KeyboardEvent("keydown",{key:"a"})),document.dispatchEvent(new KeyboardEvent("keydown",{key:"w"})),document.dispatchEvent(new KeyboardEvent("keydown",{key:"e"})),document.dispatchEvent(new KeyboardEvent("keydown",{key:"s"})),document.dispatchEvent(new KeyboardEvent("keydown",{key:"o"})),document.dispatchEvent(new KeyboardEvent("keydown",{key:"m"})),document.dispatchEvent(new KeyboardEvent("keydown",{key:"e"}));
