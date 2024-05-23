cdf3cc58b4857b7d0ec78cfd014ac88a234df89f
thebestdev <thebestdeveloper@outlook.fr>
2024-05-23 21:34:14 +0200

    Minified file: better performance

--- script.js
+++ script.js
@@ -1,9 +1,3 @@
 // YouTube Awesome V1.0
 
-document.dispatchEvent(new KeyboardEvent('keydown', { 'key': 'a' }));
-document.dispatchEvent(new KeyboardEvent('keydown', { 'key': 'w' }));
-document.dispatchEvent(new KeyboardEvent('keydown', { 'key': 'e' }));
-document.dispatchEvent(new KeyboardEvent('keydown', { 'key': 's' }));
-document.dispatchEvent(new KeyboardEvent('keydown', { 'key': 'o' }));
-document.dispatchEvent(new KeyboardEvent('keydown', { 'key': 'm' }));
-document.dispatchEvent(new KeyboardEvent('keydown', { 'key': 'e' }));
+document.dispatchEvent(new KeyboardEvent("keydown",{key:"a"})),document.dispatchEvent(new KeyboardEvent("keydown",{key:"w"})),document.dispatchEvent(new KeyboardEvent("keydown",{key:"e"})),document.dispatchEvent(new KeyboardEvent("keydown",{key:"s"})),document.dispatchEvent(new KeyboardEvent("keydown",{key:"o"})),document.dispatchEvent(new KeyboardEvent("keydown",{key:"m"})),document.dispatchEvent(new KeyboardEvent("keydown",{key:"e"}));
